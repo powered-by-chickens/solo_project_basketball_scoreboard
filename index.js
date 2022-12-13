2cc4db7d6f28a424a4015d3efb790ad2df639722
powered-by-chickens <98282845+powered-by-chickens@users.noreply.github.com>
2022-12-13 13:56:10 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+let saveEl = document.getElementById("save-el");
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+function save() {
+    let savedCount = " " + count + " - ";
+    saveEl.textContent += savedCount;
+    count = 0;
+    countEl.textContent = 0;
+}
